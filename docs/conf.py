fe36d9e5df8e3e48847faf32c8d0100b35d836d7
Max Schubach <max.schubach@bih-charite.de>
2024-09-12 16:09:06 +0200

    docs: fix

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinx_rtd_theme',
-    'sphinx_mdinclude',
     'myst_parser'
 ]
 
@@ -56,7 +55,11 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.rst']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # The master toctree document.
 master_doc = 'index'
