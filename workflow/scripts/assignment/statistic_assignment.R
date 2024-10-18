54b55653667d3c99735d5ba19eea550c1eb53b91
Max Schubach <max.schubach@bih-charite.de>
2024-10-18 15:08:19 +0200

    fix assignment statistic due to new output

--- workflow/scripts/assignment/statistic_assignment.R
+++ workflow/scripts/assignment/statistic_assignment.R
@@ -3,38 +3,38 @@ library(optparse)
 
 
 option_list <- list(
-    make_option(c("-i", "--input"),
-        type = "character",
-        help = "Assignment parcode file"
-    ),
-    make_option(c("-p", "--plot"),
-        type = "character",
-        help = "histogram plot"
-    ),
-    make_option(c("-s", "--statistic"),
-        type = "character",
-        help = "Statistics"
-    )
+  make_option(c("-i", "--input"),
+    type = "character",
+    help = "Assignment parcode file"
+  ),
+  make_option(c("-p", "--plot"),
+    type = "character",
+    help = "histogram plot"
+  ),
+  make_option(c("-s", "--statistic"),
+    type = "character",
+    help = "Statistics"
+  )
 )
 
 arguments <- parse_args(OptionParser(option_list = option_list), positional_arguments = TRUE)
 opt <- arguments$options
 
 if (!"input" %in% names(opt)) {
-    stop("--input parameter must be provided. See script usage (--help)")
+  stop("--input parameter must be provided. See script usage (--help)")
 }
 if (!"plot" %in% names(opt)) {
-    stop("--plot parameter must be provided. See script usage (--help)")
+  stop("--plot parameter must be provided. See script usage (--help)")
 }
 if (!"statistic" %in% names(opt)) {
-    stop("--statistic parameter must be provided. See script usage (--help)")
+  stop("--statistic parameter must be provided. See script usage (--help)")
 }
 
 bcs <- read_delim(opt$input, delim = "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
 
 bcs <- bcs %>%
-    filter(!(X2 %in% c("other", "ambiguous"))) %>%
-    separate(X4, c("support", "all"), sep = "/", convert = TRUE)
+  filter(!(X2 %in% c("other", "ambiguous"))) %>%
+  separate(X4, c("support", "all"), sep = "/", convert = TRUE)
 
 
 
@@ -42,13 +42,13 @@ avg_support <- mean(bcs$support / bcs$all)
 median_support <- median(bcs$support / bcs$all)
 
 bcs <- bcs %>%
-    group_by(X2) %>%
-    count() %>%
-    ungroup()
+  group_by(X2) %>%
+  count() %>%
+  ungroup()
 
 oligos_support <- bcs %>%
-    filter(n >= 15) %>%
-    nrow()
+  filter(n >= 15) %>%
+  nrow()
 
 output <- data.frame(Value = c(avg_support, median_support, oligos_support))
 
@@ -57,23 +57,25 @@ rownames(output) <- c("Average support of BC for Oligo:", "Median support of BC 
 write.table(output, file = opt$statistic, quote = FALSE, sep = "\t", row.names = TRUE, col.names = FALSE)
 
 p <- ggplot(bcs, aes(n)) +
-    geom_histogram(bins = 150) +
-    xlab("Number of BCs") +
-    ylab("Number of Oligos") +
-    geom_vline(aes(xintercept = mean(bcs$n)), col = "red", linewidth = 2) +
-    geom_text(
-        aes(
-            label = sprintf("\nmean = %.2f", mean(bcs$n)),
-            x = Inf, y = Inf
-        ), hjust = 1.1, vjust = 1.3, colour = "red"
-    ) +
-    geom_vline(aes(xintercept = median(bcs$n)), col = "blue", linewidth = 2) +
-    geom_text(
-        aes(
-            label = sprintf("median = %.2f\n", median(bcs$n)),
-            x = Inf, y = Inf
-        ), hjust = 1.1, vjust = 1.3, colour = "blue"
-    ) +
-    theme_bw()
+  geom_histogram(bins = 150) +
+  xlab("Number of BCs") +
+  ylab("Number of Oligos") +
+  geom_vline(aes(xintercept = mean(bcs$n)), col = "red", linewidth = 2) +
+  geom_text(
+    aes(
+      label = sprintf("\nmean = %.2f", mean(bcs$n)),
+      x = Inf, y = Inf
+    ),
+    hjust = 1.1, vjust = 1.3, colour = "red"
+  ) +
+  geom_vline(aes(xintercept = median(bcs$n)), col = "blue", linewidth = 2) +
+  geom_text(
+    aes(
+      label = sprintf("median = %.2f\n", median(bcs$n)),
+      x = Inf, y = Inf
+    ),
+    hjust = 1.1, vjust = 1.3, colour = "blue"
+  ) +
+  theme_bw()
 
 ggsave(opt$plot, p, type = "cairo", dpi = 300)
