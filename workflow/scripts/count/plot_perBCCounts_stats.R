bb2cc2883ca425a3b34d1e09b528d48144e8f645
Max Schubach <max.schubach@bih-charite.de>
2025-10-01 11:46:28 +0200

    spacces around infix operators

--- workflow/scripts/count/plot_perBCCounts_stats.R
+++ workflow/scripts/count/plot_perBCCounts_stats.R
@@ -94,23 +94,23 @@ for (n in 1:(data %>% nrow())) {
   intercept_median <- median(counts$DNA)
   intercept_mean <- mean(counts$DNA)
   plots_dna[[n]] <- ggplot(counts, aes(x = DNA)) +
-    geom_histogram(bins=100) +
+    geom_histogram(bins = 100) +
     geom_vline(xintercept = intercept_median, colour = "red") +
     geom_vline(xintercept = intercept_mean, colour = "blue") +
-    xlim(0,100) +
+    xlim(0, 100) +
     ggtitle(paste("replicate", data[n, ]$Replicate, sep = " "))
   intercept_median <- median(counts$RNA)
   intercept_mean <- mean(counts$RNA)
   plots_rna[[n]] <- ggplot(counts, aes(x = RNA)) +
-    geom_histogram(bins=100) +
+    geom_histogram(bins = 100) +
     geom_vline(xintercept = intercept_median, colour = "red") +
     geom_vline(xintercept = intercept_mean, colour = "blue") +
-    xlim(0,100) +
+    xlim(0, 100) +
     ggtitle(paste("replicate", data[n, ]$Replicate, sep = " "))
 }
 
 hist_plot <- do.call("plot_grid", c(plots_rna))
-ggsave(sprintf("%s_RNA_perBarcode.png", outdir), hist_plot, dpi=300, type="cairo")
+ggsave(sprintf("%s_RNA_perBarcode.png", outdir), hist_plot, dpi = 300, type = "cairo")
 
 hist_plot <- do.call("plot_grid", c(plots_dna))
-ggsave(sprintf("%s_DNA_perBarcode.png", outdir), hist_plot, dpi=300, type="cairo")
+ggsave(sprintf("%s_DNA_perBarcode.png", outdir), hist_plot, dpi = 300, type = "cairo")
